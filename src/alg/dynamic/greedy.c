423fad6df4e5758427c4250096b8b1fa34cdebb7
2lovecode <liu546hao@163.com>
2021-11-11 15:29:44 +0800

    t

---
+++ src/alg/dynamic/greedy.c
@@ -0,0 +1,22 @@
+/*
+ * 贪心算法
+ *
+ * 每一步
+ * 
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+
+
+    int arr2[2][2] = {{1, 2}, {3, 4}};
+
+    return EXIT_SUCCESS;
+}
+
+// 生成二维数组
+
+
